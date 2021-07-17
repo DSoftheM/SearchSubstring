33c262b99f6a35205503aebe456db8578d52138d
DSoftheM <80157173+DSoftheM@users.noreply.github.com>
2021-07-17 23:19:56 +0500

    Добавьте файлы проекта.

---
+++ Поиск подстроки/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Поиск_подстроки
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Поиск самой длинной подстроки, состоящей из одного и того же символа:
+            var word = "qwweeeqwweettttte"; // return ttttt
+            var result = Search(word);
+            Console.WriteLine(result);
+        }
+
+        private static string Search(string word)
+        {
+            var firstSymbol = word[0];
+            var result = "";
+            var maxCount = -1;
+            var letter = word[0];
+            var currentCount = 0;
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (firstSymbol == word[i])
+                {
+                    currentCount++;
+                    if (currentCount > maxCount)
+                    {
+                        letter = word[i];
+                        maxCount = currentCount;
+                    }
+                }
+                else
+                {
+                    currentCount = 0;
+                    firstSymbol = word[i];
+                    result = "";
+                }
+            }
+            for (var i = 0; i <= maxCount; i++)
+                result += letter;
+            return result;
+        }
+    }
+}
